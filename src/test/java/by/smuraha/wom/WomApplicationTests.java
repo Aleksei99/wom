9211dec7815d9e0e90aaf0314ae7e735cc46e7e5
Aleksei <smurahaleha@gmail.com>
2022-01-12 15:32:59 +0300

    Initial commit

---
+++ src/test/java/by/smuraha/wom/WomApplicationTests.java
@@ -0,0 +1,13 @@
+package by.smuraha.wom;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WomApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
